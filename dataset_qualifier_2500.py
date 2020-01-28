6b12920b9a434e997b54c8c594fe3b251ab12237
debanjali05 <debanjali@sda-srv04.iai.uni-bonn.de>
2020-01-28 01:31:06 +0100

    Add New Files

---
+++ dataset_qualifier_2500.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
+
+
+dataset_file = open('Dataset_SimpleQA_qualifiers.json','r')
+dataset_decode = json.load(dataset_file)
+final_tab = pd.DataFrame(data=dataset_decode)
+#print(final_tab.)
+#final_tab = json_normalize(tab)
+distribution = pd.read_csv("relation_distribution.csv",sep='|').head(50)
+
+final_questions = pd.DataFrame(columns= final_tab.columns)
+
+for item in distribution['rel']:
+        tab_questions = final_tab[final_tab.question_relation == item].head(50)
+        final_questions = pd.concat([final_questions[:],tab_questions[:]],axis=0)
+final_questions.to_json(r'Dataset_SimpleQA_qualifiers_2500.json',orient='records')
+print(final_questions.head(5))
+print("Yes")
