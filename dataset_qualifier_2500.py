07bec97f1383b5c16c82588567dc0c412348398e
debanjali05 <debanjali@sda-srv04.iai.uni-bonn.de>
2020-02-01 13:33:54 +0100

    Add Files

--- dataset_qualifier_2500.py
+++ dataset_qualifier_2500.py
@@ -8,13 +8,14 @@ dataset_decode = json.load(dataset_file)
 final_tab = pd.DataFrame(data=dataset_decode)
 #print(final_tab.)
 #final_tab = json_normalize(tab)
-distribution = pd.read_csv("relation_distribution.csv",sep='|').head(50)
+distribution = pd.read_csv("relation_distribution.csv",sep='|')
+distribution = distribution.iloc[50:100] #change the values here to select the section of the relations
 
 final_questions = pd.DataFrame(columns= final_tab.columns)
 
 for item in distribution['rel']:
-        tab_questions = final_tab[final_tab.question_relation == item].head(50)
+        tab_questions = final_tab[final_tab.question_relation == item].head(40) #change the value here to select the section of the questions
         final_questions = pd.concat([final_questions[:],tab_questions[:]],axis=0)
-final_questions.to_json(r'Dataset_SimpleQA_qualifiers_2500.json',orient='records')
+final_questions.to_json(r'Dataset_SimpleQA_qualifiers_2000.json',orient='records')
 print(final_questions.head(5))
 print("Yes")
