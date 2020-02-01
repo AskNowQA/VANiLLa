44de2e10f17dd5b59fc7a593f38d85f18616da9d
debanjali05 <debanjali@sda-srv04.iai.uni-bonn.de>
2020-02-01 14:17:29 +0100

    Add Files

--- dataset_csv.py
+++ dataset_csv.py
@@ -3,7 +3,7 @@ from pandas.io.json import json_normalize
 import json
 import csv
 
-dataset_file = open('Dataset_SimpleQA_qualifiers_2500.json','r')
+dataset_file = open('Dataset_SimpleQA_qualifiers_2000.json','r') #change the file name here to fetch the data from
 dataset_decode = json.load(dataset_file)
 tab = pd.DataFrame(data=dataset_decode, columns= ['question_id', 'question','answer_entity_labels'])
 
@@ -22,25 +22,27 @@ new_tab = new_tab.dropna(axis = 'columns', how= 'all')
 #print(new_tab.head(3))
 #print(new_tab.describe)
 
-#index = list(range(0,961))
 
-tab1 = new_tab.iloc[:961]
+index = len(new_tab.index)
+#print(index)
+
+tab1 = new_tab.iloc[:(index/2)]
 tab1.rename(columns = {'question_id':'UID1','question':'Ques1','answer_entity_labels':'Ans1'},inplace=True)
 tab1 = tab1.reset_index(drop=True)
-print (list(tab1.columns))
-print(tab1.head(3))
+#print (list(tab1.columns))
+#print(tab1.head(3))
 
-tab2 = new_tab.iloc[961:]
+tab2 = new_tab.iloc[(index/2):]
 tab2.rename(columns = {'question_id':'UID2','question':'Ques2','answer_entity_labels':'Ans2'},inplace=True)
 tab2 = tab2.reset_index(drop=True)
-print (list(tab2.columns))
-print(tab2.head(3))
+#print (list(tab2.columns))
+#print(tab2.head(3))
 
 
 final_tab = tab1.join(tab2)
-print (list(final_tab.columns))
+#print (list(final_tab.columns))
 
-final_tab.to_csv(r'Dataset_AMT.csv',encoding = 'utf-8',quoting = csv.QUOTE_ALL, index_label = 'Index')
+final_tab.to_csv(r'Dataset_batch2_AMT.csv',encoding = 'utf-8',quoting = csv.QUOTE_ALL, index_label = 'Index') #change the file name for the different batches
 
 print(final_tab.head(3))
 print(final_tab.describe)
